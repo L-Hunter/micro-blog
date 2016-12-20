79c61e531446e677ddcc6db8265151a0a629654b
TheButlerDidIt <labutler@gmail.com>
2016-12-20 17:35:44 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+ruby '2.3.3'
+
+gem 'sinatra'
+
+gem 'sendgrid-ruby'
+
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+gem 'sqlite3'
+
+gem 'rake'
+
+gem 'sinatra-flash'
---
+++ blog.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sendgrid-ruby'
+require 'sinatra/activerecord'
+require 'sinatra/flash'
+# require './models.rb'
+
+set :database, "sqlite3:test.sqlite3"
+
+get '/' do
+	erb :home
+end
